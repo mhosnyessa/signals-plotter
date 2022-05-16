221c7cf71133d19397a8a1c83209aa581d774c73
muhammed_hosny <muhammed195772@feng.bu.edu.eg>
2022-05-16 02:09:27 +0200

    example graph from the book

---
+++ lib/input_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class InputScreen extends StatelessWidget {
+  const InputScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        // body: ,
+        );
+  }
+}
