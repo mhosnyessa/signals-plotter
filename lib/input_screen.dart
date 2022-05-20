568184ba6dd222467260d542356206febb2e5d58
muhammed_hosny <muhammed195772@feng.bu.edu.eg>
2022-05-20 21:03:58 +0200

    input screen layout

--- lib/input_screen.dart
+++ lib/input_screen.dart
@@ -1,12 +1,81 @@
 import 'package:flutter/material.dart';
 
 class InputScreen extends StatelessWidget {
-  const InputScreen({Key? key}) : super(key: key);
+  InputScreen({Key? key}) : super(key: key);
+
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // body: ,
-        );
+      appBar: AppBar(
+        title: const Text('Signals Plotter'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          children: [
+            OneEntry(),
+            OneEntry(),
+            OneEntry(),
+            OneEntry(),
+          ],
+        ),
+        // child: ListView(
+        //   children: const [
+        //        OneCoordinateEntry(
+        //         isDegrees: true,
+        //       ),
+        //   ],
+        // ),
+      ),
+    );
+  }
+}
+
+class OneEntry extends StatelessWidget {
+  const OneEntry({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // height: 30,
+      // width: 30,
+      child: Row(
+        children: [
+          Flexible(child: OneCoordinateEntry(isDegrees: true)),
+          Flexible(child: OneCoordinateEntry(isDegrees: false)),
+        ],
+      ),
+    );
+  }
+}
+
+class OneCoordinateEntry extends StatelessWidget {
+  const OneCoordinateEntry({
+    Key? key,
+    required this.isDegrees,
+  }) : super(key: key);
+
+  final bool isDegrees;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: Center(
+        child: TextFormField(
+          decoration: InputDecoration(
+            // icon: Icon(Icons.send),
+            hintText: isDegrees ? 'degree (0:360)' : 'Voltage (v)',
+            // helperText: 'Helper Text',
+            // counterText: '0 characters',
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:signals_plotter/input_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,37 +16,52 @@ class MyApp extends StatelessWidget {
       [62, 35, -38, -64, -63, -52, -28, 24, 80, 96, 90, 70]
     ];
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          // child: Text('Hi Mom'),
-          child: LineChart(
-            LineChartData(
-              lineBarsData: <LineChartBarData>[
-                LineChartBarData(
-                    show: true,
-                    isCurved: true,
-                    spots: () {
-                      List<FlSpot> flSpots = [];
-                      for (var i = 0; i < data[0].length; i++) {
-                        flSpots.add(FlSpot(
-                          data[0][i].toDouble(),
-                          data[1][i].toDouble(),
-                        ));
-                      }
-                      return flSpots;
-                    }()
-                    // spots: List<FlSpot>.from(data[0].map((e) {
-                    //   return FlSpot(e.toDouble(), e.toDouble());
-                    // }).toList()),
-                    // isCurved: true
-                    ),
-              ],
-              minX: 30,
-              maxX: 360,
-              // Y
-              minY: -100,
-              maxY: 100,
-            ),
+      // home: ResultScreen(data: data),
+      home: InputScreen(),
+    );
+  }
+}
+
+class ResultScreen extends StatelessWidget {
+  const ResultScreen({
+    Key? key,
+    required this.data,
+  }) : super(key: key);
+
+  final List data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        // child: Text('Hi Mom'),
+        child: LineChart(
+          LineChartData(
+            lineBarsData: <LineChartBarData>[
+              LineChartBarData(
+                  show: true,
+                  isCurved: true,
+                  spots: () {
+                    List<FlSpot> flSpots = [];
+                    for (var i = 0; i < data[0].length; i++) {
+                      flSpots.add(FlSpot(
+                        data[0][i].toDouble(),
+                        data[1][i].toDouble(),
+                      ));
+                    }
+                    return flSpots;
+                  }()
+                  // spots: List<FlSpot>.from(data[0].map((e) {
+                  //   return FlSpot(e.toDouble(), e.toDouble());
+                  // }).toList()),
+                  // isCurved: true
+                  ),
+            ],
+            minX: 30,
+            maxX: 360,
+            // Y
+            minY: -100,
+            maxY: 100,
           ),
         ),
       ),
